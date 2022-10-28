523d24521f6d3e121d01116ee4da10bf2d58f7aa
Mikhail Sushchikh <sumux200013@gmail.com>
2022-10-28 07:08:21 +0400

    link changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
-}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","C:/webdrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://lichess.org/";
+        driver.get(baseUrl);
+        }
+    }
